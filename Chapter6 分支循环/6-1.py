b0df2edc08ad164afbbdb6108aa5163d028ccf6c
Emp <Zhanyuan1991.chen@gmail.com>
2019-04-03 15:53:34 +1100

    added 6.1 and 6.2

--- Chapter6 分支循环/6-1.py
+++ Chapter6 分支循环/6-1.py
@@ -1,1 +1,24 @@
+'''
+第六章 分支循环 条件与枚举
+
+6-1  什么是表达式
+
+表达式(expression) 是运算符(operator) 和操作数(operand)所构成的序列。
+
+序列 是多个元素构成的且有顺序
+
+1+2*3 要按照顺序计算  按照优先级计算
+a=1+2*3 也是表达式
+
+a=1  b=2  c=3 
+a+b*c ===> ?  先乘除后加减  1+6=7
+
+a or b and c ===> ?
+b and c ===> 2 and 3 = True   2 and 3 = 3 
+1 or True ===> True     1 or 3 = 1
+
+
+
+
+'''
 print("Hello world")
